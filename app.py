28bbe7d3a48a2679d42f9a010b9e22b7e3df8016
tyagi2607 <tyagi_26@yahoo.com>
2023-05-12 20:27:32 -0700

    added dashboard layout

--- app.py
+++ app.py
@@ -2,6 +2,48 @@
 import streamlit as st
 import pandas as pd
 import pymongo
+import plotly.express as px
+
+def show_overview(client, coin):
+    # Show the content for the Overview page
+    # Select database and collection
+    db = client["bitcoin_database"]
+    collection = db["bitcoin_market"]
+
+
+    data = []
+    for document in collection.find():
+        data.append(document["price"])
+
+    df = pd.DataFrame(data, columns=["Bitcoin Price (USD)"])
+
+    st.title(coin)
+    st.markdown(coin)
+    st.markdown("Details")
+    st.table(df.head())
+    st.line_chart(df)
+    
+    # Display the coin overview
+    st.subheader(coin)
+    st.write("Current Price: $")
+    st.write("Market Cap: $")
+    st.write("Volume: $")
+    # Add card visual, table, or any other desired components
+
+def show_profile():
+    # Show the content for the Profile page
+    st.title("Profile")
+    # Add card visual, table, or any other desired components
+
+def show_charts():
+    # Show the content for the Charts page
+    st.title("Charts")
+    # Add card visual, table, or any other desired components
+
+def show_metrics():
+    # Show the content for the Metrics page
+    st.title("Metrics")
+    # Add card visual, table, or any other desired components
 
 def main():
     # STREAMLIT: Page configuration
@@ -10,6 +52,7 @@ def main():
     page_icon="✅",
     layout="wide",
     )
+    
 
     # FUNCTION: Connect to MongoDB
     @st.cache_resource
@@ -24,31 +67,37 @@ def main():
 
     client = init_connection()
     # Select database and collection
-    db = client["bitcoin_database"]
-    collection = db["bitcoin_market"]
-
 
     #def init_connection():
     #    return pymongo.MongoClient("mongodb+srv://st.secrets.db_username:st.secrets.db_password@st.secrets.cluster_name.a0jvoed.mongodb.net/st.secrets.db_name?retryWrites=true&w=majority")
 
     # client = init_connection()
 
-
-    data = []
-    for document in collection.find():
-        data.append(document["price"])
-
-    df = pd.DataFrame(data, columns=["Bitcoin Price (USD)"])
-
-    st.sidebar.title("more coins")
-    st.sidebar.markdown("Welcome!!!")
-    st.sidebar.checkbox("Show Analysis", True, key=1)
-    select = st.sidebar.selectbox('Select a State',["Bitcoin"])
     st.title("Coin metrics")
     st.markdown("The dashboard will visualize the coin metrics")
     st.markdown("Details")
-    st.table(df.head())
-    st.line_chart(df)
+
+    st.sidebar.title("more coins")
+    st.sidebar.markdown("Welcome!!!")
+    st.sidebar.checkbox("Show Analysis", True, key=2)
+
+    # Create a sidebar for coin selection and page navigation
+    coin = st.sidebar.selectbox("Select Coin", ["Bitcoin", "Ethereum", "Litecoin"])
+
+    # Create radio buttons for selecting the page
+    page = st.sidebar.radio("Page", ["Overview", "Profile", "Charts", "Metrics"])
+
+
+
+    # Based on the selected page, show the corresponding content
+    if page == "Overview":
+        show_overview(client, coin)
+    elif page == "Profile":
+        show_profile()
+    elif page == "Charts":
+        show_charts()
+    elif page == "Metrics":
+        show_metrics()
 
 if __name__ == "__main__":
     main()
